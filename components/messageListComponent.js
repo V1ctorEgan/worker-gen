822763cccc834377a2e536c5640e5cf88a36f8fe
USER PC <victorifeanyi2004@gmail.com>
2024-12-20 15:18:01 +0100

    added hover effect

--- components/messageListComponent.js
+++ components/messageListComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MessageListComponent({Name, Topic, Status, Created}){
     return(
-        <div className='w-full text-xs pl-2 border-t-2 p-2 flex border-gray-100 cursor-pointer'>
+        <div className='w-full text-xs pl-2 border-t-2 p-2 flex border-gray-100 cursor-pointer hover:bg-gray-200'>
             <div className='mr-[9rem]'><input type='checkbox' className='cursor-pointer' /> <span className='text-blue-500 '>{Name}</span></div>
             <div className='mr-[4rem] text-gray-500'>{Topic}</div>
             <div className='mr-[15rem]'>{Status}</div> 
